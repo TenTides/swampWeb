5fe79836e7428b8b271629e88f345368303c1331
Nelson Daniel Herrera Gamboa <96972167+NlsnBoa@users.noreply.github.com>
2023-01-29 05:34:43 +0000

    created loading screen and updated html, css, js

--- Web/static/app.js
+++ Web/static/app.js
@@ -1,8 +1,11 @@
-const filled = document.querySelector('.filled');
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
 
-function update () {
-  filled.style.width = `${(window.scrollY) / (document.body.scollHeight - window.innerHeight) * 100}%`
-  requestAnimationFrame();
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
 }
 
-update();
