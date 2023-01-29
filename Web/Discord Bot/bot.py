a798bb1c657b353da7f759ad997bb6f36ca2d223
Nelson Daniel Herrera Gamboa <96972167+NlsnBoa@users.noreply.github.com>
2023-01-29 09:00:42 +0000

    changed bot

--- Web/Discord Bot/bot.py
+++ Web/Discord Bot/bot.py
@@ -1,5 +1,7 @@
 import discord
 import responses
+import asyncio
+
 
 # async is going to send a message to the current channel or the user
 async def send_message(message, user_message, is_private):
@@ -38,11 +40,75 @@ def run_discord_bot():
 
         #this checks whether the use typed a message with question mark 
         # this means that you want to have the response sent in private.
-        if user_message[0] == '?':
+        if len(user_message) != 0 and user_message[0] == '?':
             user_message = user_message[1:]
             await send_message(message, user_message, is_private=True)
         else:
             await send_message(message, user_message, is_private=False)
+
+    client.run(TOKEN)
+
+# //////////
+
+async def run():
+    intents = discord.Intents(members=True)
+    client=discord.Client(intents=intents)
+    welcomechannel = await client.fetch_channel(1069011685869895730) 
+
+    @client.event
+    async def on_ready():
+        print('logged in as')
+        print(client.user.name)
+        print(client.user.id)
+        print('-----')
+        # Customise the message below to what you want to send new users!
+        newUserMessage = """
+        You
+        can
+        put
+        your
+        multiline
+        message
+        here!
+        """
+
+    @client.event
+    async def on_member_join(member):
+        print("Recognised that a member called " + member.name + " joined")
+        try: 
+            await client.send_message(member, newUserMessage)
+            print("Sent message to " + member.name)
+        except:
+            print("Couldn't message " + member.name)
+            embed=discord.Embed(
+            title="Welcome "+ member.name + "!",
+            description="We're so glad you're here!",
+            color=discord.Color.green()
+        )
+            
+        role = discord.utils.get(member.server.roles, name="name-of-your-role") #  Gets the member role as a `role` object
+        await client.add_roles(member, role) # Gives the role to the user
+        print("Added role '" + role.name + "' to " + member.name)
+
+    TOKEN = 'MTA2ODk5NTY4MDc0OTc2ODc2Nw.GFEp_L.qAdGeG8pRBKJMbhoyQxk0Axe8TZGWJlOyVRXBM'
+
+
+
+    client.run(TOKEN)
+
+    @client.event
+    async def on_member_leave(member):
+        print("Recognised that a member called " + member.name + " left")
+        embed=discord.Embed(
+            title="😢 Goodbye "+member.name+"!",
+            description="Until we meet again old friend.", # A description isn't necessary, you can delete this line if you don't want a description.
+            color=discord.Color.red() # There are lots of colors, you can check them here: https://discordpy.readthedocs.io/en/latest/api.html?highlight=discord%20color#discord.Colour
+        )
+
+
+    TOKEN = 'MTA2ODk5NTY4MDc0OTc2ODc2Nw.GFEp_L.qAdGeG8pRBKJMbhoyQxk0Axe8TZGWJlOyVRXBM'
+
+
 
     client.run(TOKEN)
 
